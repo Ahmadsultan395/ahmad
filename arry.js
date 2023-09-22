e3349d554c408a6656db5fe88ee0193b489c9d9a
Ahmadsultan395 <145713175+Ahmadsultan395@users.noreply.github.com>
2023-09-22 14:09:58 -0700

    first commit

---
+++ arry.js
@@ -0,0 +1,56 @@
+
+let ary =[];
+let fruit=["apple","mango","banan"];
+fruit[0]="paiapple";
+fruit[1]="slait";
+fruit[2]="habrid";
+alert(fruit[0]);
+alert("this is a"+fruit[0]);
+let name =(firstname="ahmad", lastname="sultan");
+alert(fruit.length);
+alert(fruit)
+// last index
+alert(fruit.length-1);
+// variable write for twice 
+alert(fruit.at(-1));
+let fruit=["apple","mango","banan"];
+// push append element to end 
+alert(fruit.push);
+// pop take element from end
+alert(fruit.pop);
+// shift get form Start
+alert(fruit.shift);
+// unshift add elemnt to start
+alert(fruit.unshift);
+//  for loop
+let fruit=["apple","mango","banan"];
+for(let i =0; i<fruit.length; i++){
+    alert(fruit);
+}
+// for of
+let fruit=["apple","mango","banan"];
+for(let arr of fruit){
+    alert(fruit)
+}
+// for in
+for(let key in fruit){
+    alert(fruit[arr]);
+}
+// clear arry
+arry.length=0;
+// new arrry method 
+let arr = new Array ();
+// mutidimentional arry
+let martix = [
+    [1,2],
+    [3,4]
+];
+alert(martix[2][1]); 
+// to string   retune arry element with commaa
+let martix = ['1', '2','3', '4','6'];
+        alert(String(martix));
+
+
+
+
+
---
+++ gridlayout.js
@@ -0,0 +1,2 @@
+let a=document.getElementsByClassName('cantainer').style.color="black";
+document.write(a);
