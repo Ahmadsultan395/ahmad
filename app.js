3cc7e8e08e766447667753b635d0626afd0f0426
Ahmadsultan395 <145713175+Ahmadsultan395@users.noreply.github.com>
2023-09-23 14:12:55 -0700

    ..

---
+++ app.js
@@ -0,0 +1,14 @@
+const hamburger = document.getElementById('hamburger');
+const navbar = document.getElementById('navbar');
+
+hamburger.addEventListener('click', function () {
+    if (!navbar.style.display) {
+        navbar.style.display = 'flex'
+        hamburger.textContent = 'Hide'
+    }
+    else {
+        navbar.style.display = ''
+        hamburger.textContent = 'Show'
+    }
+
+})
