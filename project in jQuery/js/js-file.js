693d7b7b744389ba4ff69ffdbba847d100724963
Ahmadsultan395 <145713175+Ahmadsultan395@users.noreply.github.com>
2023-11-09 07:49:17 -0800

    new

--- project in jQuery/js/js-file.js
+++ project in jQuery/js/js-file.js
@@ -5,22 +5,22 @@
 
 // hero section
 $(document).ready(function () {
-  
+
     // background color
-    var herosection=document.querySelector('.hero-section');
-    herosection.addEventListener("mouseover",function(){
-            document.querySelector('.hero-section').style.background="#8510cddd";
+    var herosection = document.querySelector('.hero-section');
+    herosection.addEventListener("mouseover", function () {
+        document.querySelector('.hero-section').style.background = "#8510cddd";
     });
-    herosection.addEventListener("mouseout",function(){
-            document.querySelector('.hero-section').style.background="#024571";
+    herosection.addEventListener("mouseout", function () {
+        document.querySelector('.hero-section').style.background = "#024571";
     });
 
-    $('.hero-section-content').mouseover(function(){
+    $('.hero-section-content').mouseover(function () {
         $('.hero-section-title h1').animate({
-            top:'3000px',
-        },3000); 
+            top: '3000px',
+        }, 3000);
     });
-    
+
 });
 
 // end
@@ -57,37 +57,56 @@ $(document).ready(function () {
     })
 });
 // end
+// ========================/
+
 
 // form validation
-
+$(document).ready(function () {
 
 $("#myform").validate({
-rules:{
-    name:{
-        minlength: 2,
+    rules: {
+        name: {
+            minlength: 2,
+        },
+        phone: {
+            number:true,
+            minlength: 11,
+            maxlength: 11,
+        },
+        email: {
+            email: true,
+        },
+        Message: {
+            minlength: 2,
+        }
     },
-    email:{
-        email:true,
-    },
-    phone:{
-        Number:true,
-        minlength:11,
-        maxlength:11, 
-    },
-},
 
-messages:{
-    name:{
-    required: " Please Enter your name",
-    minlength: jQuery.validator.format("Please  at least 2 character")
-    },
+    messages: {
+        name: {
+            required: " Please Enter your name",
+            minlength: "Please  at least 2 character"
+        },
+        phone: {
+        required:"please enter a valid phone number",
+            minlength:"not complete number",
+        },
+
         email: "please enter a valid email",
-        phone: "please enter a valid phone number",
-},
+        Message: {
+            required: "This field is required.",
+            minlength: "Please  at least 10 character"
+        }
+    },
 
 
 
-    submitHandler: function(form) {
-      form.submit();
+    submitHandler: function (form) {
+        form.submit();
     }
-   });
+});
+
+});
+
+//    end
+// ========================
+
